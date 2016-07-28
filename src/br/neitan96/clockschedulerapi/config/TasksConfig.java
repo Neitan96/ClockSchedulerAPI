f831c7ba16eaf9d7cde18d64d727de65416c7280
Nathan Almeida <nathanalmeidadev@outlook.com>
2016-07-28 20:08:43 -0300

    Gerenciador de tasks em configs adicionado

---
+++ src/br/neitan96/clockschedulerapi/config/TasksConfig.java
@@ -0,0 +1,91 @@
+package br.neitan96.clockschedulerapi.config;
+
+import br.neitan96.clockschedulerapi.ClockSchedulerAPI;
+import br.neitan96.clockschedulerapi.sheduler.TaskCommand;
+import org.bukkit.configuration.InvalidConfigurationException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Neitan96 on 26/07/2016.
+ */
+public class TasksConfig{
+
+    protected final String path;
+    protected final File file;
+    protected final YamlConfigurationUTF8 configuration = new YamlConfigurationUTF8();
+    protected final List<TaskCommand> taskCommands = new ArrayList<>();
+
+    public TasksConfig(String path, File file) throws IOException, InvalidConfigurationException{
+        this.path = path;
+        this.file = file;
+        load();
+    }
+
+    public TasksConfig(File file) throws IOException, InvalidConfigurationException{
+        this("Comandos", file);
+    }
+
+    public void load() throws IOException, InvalidConfigurationException{
+        configuration.load(file);
+
+        for(String alarmCommandStr : configuration.getStringList(path)){
+
+            TaskCommand taskCommand = TaskCommand.fromString(
+                    ClockSchedulerAPI.getInstance(), alarmCommandStr);
+
+            taskCommands.add(taskCommand);
+        }
+    }
+
+    protected void exportAlarmsToConfig(){
+        ArrayList<String> alarmsString = taskCommands.stream()
+                .map(TaskCommand::toString)
+                .collect(Collectors.toCollection(ArrayList::new));
+        configuration.set(path, alarmsString);
+    }
+
+    public List<TaskCommand> getAlarms(){
+        return Collections.unmodifiableList(taskCommands);
+    }
+
+    public void add(TaskCommand taskCommand, boolean register){
+        if(taskCommands.contains(taskCommand)){
+            taskCommands.add(taskCommand);
+            exportAlarmsToConfig();
+        }
+    }
+
+    public void remove(int index){
+        taskCommands.remove(index);
+        exportAlarmsToConfig();
+    }
+
+    public void remove(TaskCommand taskCommand){
+        taskCommands.remove(taskCommand);
+        exportAlarmsToConfig();
+    }
+
+    public void save() throws IOException{
+        configuration.save(file);
+    }
+
+    public void save(File file) throws IOException{
+        configuration.save(file);
+    }
+
+    public void save(OutputStream stream) throws IOException{
+        configuration.save(stream);
+    }
+
+    public String saveToString(){
+        return configuration.saveToString();
+    }
+
+}
