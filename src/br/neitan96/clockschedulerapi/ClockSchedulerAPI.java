9daf49e2199f4544d775ce9185e7e3f91e4e89ea
Nathan Almeida <nathanalmeidadev@outlook.com>
2016-08-13 17:56:38 -0300

    Classe principal atualizada

--- src/br/neitan96/clockschedulerapi/ClockSchedulerAPI.java
+++ src/br/neitan96/clockschedulerapi/ClockSchedulerAPI.java
@@ -2,7 +2,18 @@ package br.neitan96.clockschedulerapi;
 
 import br.neitan96.clockschedulerapi.alarms.ClockAlarm;
 import br.neitan96.clockschedulerapi.alarms.converters.AlarmConvetors;
-import br.neitan96.clockschedulerapi.commands.*;
+import br.neitan96.clockschedulerapi.commands.CReload;
+import br.neitan96.clockschedulerapi.commands.debug.CDebug;
+import br.neitan96.clockschedulerapi.commands.debug.CStatus;
+import br.neitan96.clockschedulerapi.commands.debug.CTest;
+import br.neitan96.clockschedulerapi.commands.debug.CTestStress;
+import br.neitan96.clockschedulerapi.commands.tasks.*;
+import br.neitan96.clockschedulerapi.commands.time.CSetTime;
+import br.neitan96.clockschedulerapi.commands.time.CSetTimezone;
+import br.neitan96.clockschedulerapi.commands.time.CTime;
+import br.neitan96.clockschedulerapi.commands.tmptaks.CTmpTaskAdd;
+import br.neitan96.clockschedulerapi.commands.tmptaks.CTmpTaskRemove;
+import br.neitan96.clockschedulerapi.commands.tmptaks.CTmpTasks;
 import br.neitan96.clockschedulerapi.config.TasksConfig;
 import br.neitan96.clockschedulerapi.sheduler.ClockTask;
 import br.neitan96.clockschedulerapi.sheduler.TaskCommand;
@@ -16,6 +27,7 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -34,7 +46,9 @@ public class ClockSchedulerAPI extends JavaPlugin{
 
 
     @Override
-    public void onEnable() {
+    public void onEnable(){
+
+        TAG = getDescription().getName();
 
         clockSchedulerAPI = this;
         taskManager = new TaskManager();
@@ -43,19 +57,68 @@ public class ClockSchedulerAPI extends JavaPlugin{
         saveIfNotExists("exemplos.yml");
         saveIfNotExists("config.yml");
 
-        reloadConfig();
+        reloadConfig(true);
 
-        TAG = "[" + getDescription().getName() + "] ";
+        log("Registrando comandos...");
+        getCommand("clockstatus").setExecutor(new CStatus());
+        getCommand("clocktime").setExecutor(new CTime());
+        getCommand("clocksettime").setExecutor(new CSetTime());
+        getCommand("clocksettimezone").setExecutor(new CSetTimezone());
+        getCommand("clockdebug").setExecutor(new CDebug());
+        getCommand("clocktest").setExecutor(new CTest());
+        getCommand("clockteststress").setExecutor(new CTestStress());
+
+        CTmpTasks cTmpTasks = new CTmpTasks();
+        getCommand("clocktmptasks").setExecutor(cTmpTasks);
+        getCommand("clocktmptasksadd").setExecutor(new CTmpTaskAdd(cTmpTasks));
+        getCommand("clocktmptasksremove").setExecutor(new CTmpTaskRemove(cTmpTasks));
+
+        CTasks cTasks = new CTasks();
+        getCommand("clocktasks").setExecutor(cTasks);
+        getCommand("clocktasksadd").setExecutor(new CTasksAdd());
+        getCommand("clocktasksremove").setExecutor(new CTasksRemove(cTasks));
+        getCommand("clocktasksdisable").setExecutor(new CTasksDisable(cTasks));
+        getCommand("clocktasksenable").setExecutor(new CTasksEnable(cTasks));
+
+        CTasksDisableAll cTasksDisableAll = new CTasksDisableAll();
+        getCommand("clocktasksdisableall").setExecutor(cTasksDisableAll);
+        getCommand("clocktasksenablell").setExecutor(new CTasksEnableAll(cTasksDisableAll));
+        getCommand("clockreload").setExecutor(new CReload());
+
+        log("ClockSchedulerAPI iniciado!");
+    }
+
+    @Override
+    public void onDisable(){
+        taskManager.removeAll();
+        HandlerList.unregisterAll(this);
+        log("ClockSchedulerAPI terminado!");
+    }
+
+    @Override
+    public void reloadConfig(){
+        reloadConfig(false);
+    }
+
+    @Override
+    public void saveConfig(){
+        saveDebugFlagsToConfig();
+        saveCalendarToConfig();
+        saveTasksToConfig();
+    }
+
+
+    public synchronized void reloadConfig(boolean log){
+        super.reloadConfig();
 
         List<String> debugTagsList = getConfig().getStringList("Debug");
         if(debugTagsList != null){
-            log("Definindo o debug...");
+            if(log) log("Definindo o debug...");
             String[] debugTags = debugTagsList.toArray(new String[debugTagsList.size()]);
             ClockDebug.setTags(debugTags);
         }
 
-
-        log("Definindo fuso horário...");
+        if(log) log("Definindo fuso horário...");
         ClockCalendar.defaultTimeZone = TimeZone.getTimeZone(
                 getConfig().getString("FusoHorario.TimeZone", "America/Sao_Paulo")
         );
@@ -64,34 +127,47 @@ public class ClockSchedulerAPI extends JavaPlugin{
         ClockCalendar.ajusteMinutes = getConfig().getInt("FusoHorario.Ajuste.Minutos", 0);
         ClockCalendar.ajusteSeconds = getConfig().getInt("FusoHorario.Ajuste.Segundos", 0);
 
-        log("Registrando comandos...");
-        CTempComando tempCommand = new CTempComando();
-        getCommand("clockhoracerta").setExecutor(new CHoraCerta());
-        getCommand("clockteste").setExecutor(new CTeste());
-        getCommand("clocktempcomando").setExecutor(tempCommand);
-        getCommand("clockpermcomando").setExecutor(new CPermComando());
-        getCommand("clockdesativar").setExecutor(new CDesativar());
-        getCommand("clockremover").setExecutor(new CRemover());
-        getCommand("clockcomandos").setExecutor(new CComandos());
-        getCommand("clockdesativarperms").setExecutor(new CDesativarPerms());
-        getCommand("clockreativarperms").setExecutor(new CReativarPerms());
-        getCommand("clockdesativartemps").setExecutor(new CDesativarTemps(tempCommand));
-        getCommand("clockdebuglista").setExecutor(new CDebugLista());
-        getCommand("clocklimparcache").setExecutor(new CLimparCache());
-        getCommand("clockreload").setExecutor(new CReload());
-
-        log("Registrando alarmes dos comandos...");
+        if(log) log("Registrando alarmes dos comandos...");
+        if(tasksConfig != null && taskManager != null)
+            tasksConfig.getTasks().forEach(ClockTask::disable);
         File configFile = new File(getDataFolder(), "comandos.yml");
         tasksConfig = new TasksConfig(configFile);
+        tasksConfig.getTasks().forEach(ClockSchedulerAPI::addTask);
 
-        log("ClockSchedulerAPI iniciado!");
     }
 
-    @Override
-    public void onDisable() {
-        taskManager.removeAll();
-        HandlerList.unregisterAll(this);
-        log("ClockSchedulerAPI terminado!");
+    public void saveDebugFlagsToConfig(){
+        String[] debugTags = ClockDebug.getDebugTags();
+        getConfig().set("Debug", debugTags);
+        super.saveConfig();
+    }
+
+    public void saveCalendarToConfig(){
+        if(ClockCalendar.defaultTimeZone != null)
+            getConfig().set("FusoHorario.TimeZone", ClockCalendar.defaultTimeZone.getID());
+        else
+            getConfig().set("FusoHorario.TimeZone", null);
+
+        getConfig().set("FusoHorario.Ajuste.Dias", ClockCalendar.ajusteDays);
+        getConfig().set("FusoHorario.Ajuste.Horas", ClockCalendar.ajusteHours);
+        getConfig().set("FusoHorario.Ajuste.Minutos", ClockCalendar.ajusteMinutes);
+        getConfig().set("FusoHorario.Ajuste.Segundos", ClockCalendar.ajusteSeconds);
+
+        super.saveConfig();
+    }
+
+    public void saveTasksToConfig(){
+        if(tasksConfig != null){
+            try{
+
+                tasksConfig.save();
+            }catch(IOException e){
+                e.printStackTrace();
+                log("ERRO ao salvar as tasks na config!");
+            }
+        }
+
+        super.saveConfig();
     }
 
 
@@ -102,7 +178,7 @@ public class ClockSchedulerAPI extends JavaPlugin{
     }
 
 
-    public static ClockSchedulerAPI getInstance() {
+    public static ClockSchedulerAPI getInstance(){
         return clockSchedulerAPI;
     }
 
@@ -125,18 +201,20 @@ public class ClockSchedulerAPI extends JavaPlugin{
         taskManager.addTask(task);
     }
 
-    public static void addTask(ClockAlarm alarm, Runnable runnable, JavaPlugin plugin){
+    public static ClockTask addTask(ClockAlarm alarm, Runnable runnable, JavaPlugin plugin){
         ClockTask task = new ClockTask(plugin, runnable, alarm);
         addTask(task);
+        return task;
     }
 
-    public static void addTask(ClockAlarm alarm, String command, JavaPlugin plugin){
+    public static ClockTask addTask(ClockAlarm alarm, String command, JavaPlugin plugin){
         ClockTask task = new TaskCommand(plugin, command, alarm);
         addTask(task);
+        return task;
     }
 
-    public static void addTask(String alarm, Runnable runnable, JavaPlugin plugin){
-        addTask(AlarmConvetors.convert(alarm), runnable, plugin);
+    public static ClockTask addTask(String alarm, Runnable runnable, JavaPlugin plugin){
+        return addTask(AlarmConvetors.convert(alarm), runnable, plugin);
     }
 
 
@@ -150,11 +228,11 @@ public class ClockSchedulerAPI extends JavaPlugin{
 
 
     public static void log(String msg){
-        Bukkit.getConsoleSender().sendMessage(COLOR_TAG + TAG + COLOR_TEXT + msg);
+        Bukkit.getConsoleSender().sendMessage(COLOR_TAG + "[" + TAG + "]" + COLOR_TEXT + msg);
     }
 
     public static void log(CommandSender sender, String msg){
-        sender.sendMessage(COLOR_TAG + TAG + COLOR_TEXT + msg);
+        sender.sendMessage(COLOR_TAG + "[" + TAG + "] " + COLOR_TEXT + msg);
     }
 
 }
